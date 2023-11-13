f3516fdd8091146034b2c6d9a43d24a2f8ed279e
Stephen Muiru <stephen.muiru22@students.dkut.ac.ke>
2023-11-13 23:04:51 +0300

    implemented a linked in java

--- linkedList/src/Main.java
+++ linkedList/src/Main.java
@@ -1,15 +1,35 @@
 public class Main {
+
+    public  static singlyLinkList createNode( int data)
+    {
+        singlyLinkList node = new singlyLinkList(data);
+        return  node;
+    }
     public static void main(String[] args)
     {
-        singlyLinkList head = new singlyLinkList(10);
+        singlyLinkList head = new singlyLinkList();
+        singlyLinkList temp = new singlyLinkList();
 
         int i = 0;
         while (i < 5)
         {
-
-
+            singlyLinkList node  = createNode(30);
+                if(i == 0)
+                {
+                    temp = head = node;
+                }
+                else
+                {
+                    temp.next = node;
+                    temp = node;
+                }
             i++;
         }
+        temp = head;
+      do {
+            System.out.print(temp.data+"\t");
+            temp = temp.next;
+        }  while(temp.next != null);
 
     }
 }
--- linkedList/src/singlyLinkList.java
+++ linkedList/src/singlyLinkList.java
@@ -1,7 +1,13 @@
 public class singlyLinkList {
-    private int data;
-    private singlyLinkList next;
+     public int data;
+     public singlyLinkList next;
 
+
+    singlyLinkList()
+    {
+        this.data = 0;
+        this.next = null;
+    }
     public singlyLinkList(int data)
     {
         this.data = data;
